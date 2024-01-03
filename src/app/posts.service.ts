d5bceb1b9d2262ca2eb272f0e104a2cff1ca734b
beattris <beatriceebirim@gmail.com>
2024-01-03 11:17:47 +0100

    added query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -24,10 +24,14 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty')
+    searchParams = searchParams.append('custom', 'key')
     return this.http
       .get<{[key: string]: Post}>(
         'https://making-http-request-1db0d-default-rtdb.firebaseio.com/posts.json', {
-          headers: new HttpHeaders({ 'Custom-Header': 'Hello'})
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello'}),
+          params: searchParams
         }
       )
       .pipe(map(responseData => {
