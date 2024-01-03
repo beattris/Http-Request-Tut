7617f03b9472ce5291de22e331821648419f6691
beattris <beatriceebirim@gmail.com>
2024-01-03 03:38:27 +0100

    outsourced the http logic to a service, keeping component lean

---
+++ src/app/posts.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+  constructor(private http: HttpClient)  {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = {title: title, content: content}
+    this.http
+      .post<{ name: string }>(
+        'https://making-http-request-1db0d-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    return this.http
+      .get<{[key: string]: Post}>(
+        'https://making-http-request-1db0d-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(map(responseData => {
+        // using pipe to transform the response data
+        const postsArray: Post[] = [];
+        for (const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id: key})
+          }
+        }
+        return postsArray;
+      }))
+  }
+}
